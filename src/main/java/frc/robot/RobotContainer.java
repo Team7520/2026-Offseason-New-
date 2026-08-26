6341a44cae837f55c6af86c1b68f118785873f15
Anton Dunbar <thedunbarboys@gmail.com>
2026-08-26 14:30:17 -0400

    Added manual blocker commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,41 +92,51 @@ public class RobotContainer {
             .turnHood(0.1))
             .onFalse(new InstantCommand(() -> turret.stopAll()) );
 
-        driver
-        .rightBumper()
-        .whileTrue(
-            dyerotor
-            .intakeDyeAndWheel(0.5, -0.6))
-            .onFalse(new InstantCommand(() -> dyerotor.stopAll()) );
+        driver.rightBumper().whileTrue(
+            dyerotor.intakeDyeAndWheel(0.5, -0.6)
+        ).onFalse(
+            new InstantCommand(() -> dyerotor.stopAll()) 
+        );
 
-        driver
-        .leftBumper()
-        .whileTrue(
-            turret
-            .turnHood(-0.1))
-            .onFalse(new InstantCommand(() -> turret.stopAll()) );
+        driver.leftBumper().whileTrue(
+            turret.turnHood(-0.1)
+        ).onFalse(
+            new InstantCommand(() -> turret.stopAll())
+        );
+
+        driver.a().whileTrue(
+            intake.blockerManual(0.05)
+        ).onFalse(
+            intake.blockerManual(0.0)
+        );
+
+        driver.b().whileTrue(
+            intake.blockerManual(-0.05)
+        ).onFalse(
+            intake.blockerManual(0.0)
+        );
 
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
-        final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
+        // final var idle = new SwerveRequest.Idle();
+        // RobotModeTriggers.disabled().whileTrue(
+        //     drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+        // );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // joystick.b().whileTrue(drivetrain.applyRequest(() ->
+        //     point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        // ));
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        // // Run SysId routines when holding back/start and X/Y.
+        // // Note that each routine should be run exactly once in a single log.
+        // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        // // Reset the field-centric heading on left bumper press.
+        // joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
